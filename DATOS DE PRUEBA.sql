d31ee55680c6e5c9a9777302ca6fa495fce9ce2d
Kari <camargo.kari@gmail.com>
2017-04-08 18:35:09 -0300

    add values into Envios and carga table and modify paises table

--- DATOS DE PRUEBA.sql
+++ DATOS DE PRUEBA.sql
@@ -110,67 +110,17 @@ INSERT INTO factorMultipVin (posicion, factor) VALUES
 
 --Paises
 INSERT INTO Paises VALUES(1, 'Argentina');
-INSERT INTO Paises VALUES(2, 'Australia');
-INSERT INTO Paises VALUES(3, 'Barbados');
-INSERT INTO Paises VALUES(4, 'Bolivia');
-INSERT INTO Paises VALUES(5, 'Brasil');
-INSERT INTO Paises VALUES(6, 'Canadá');
-INSERT INTO Paises VALUES(7, 'Chile');
+INSERT INTO Paises VALUES(2, 'Alemania');
+INSERT INTO Paises VALUES(3, 'Italia');
+INSERT INTO Paises VALUES(4, 'Corea del Sur');
+INSERT INTO Paises VALUES(5, 'Japón');
+INSERT INTO Paises VALUES(6, 'Francia');
+INSERT INTO Paises VALUES(7, 'Mexico');
 INSERT INTO Paises VALUES(8, 'China');
-INSERT INTO Paises VALUES(9, 'Colombia');
-INSERT INTO Paises VALUES(10, 'Corea del Norte');
-INSERT INTO Paises VALUES(11, 'Corea del Sur');
-INSERT INTO Paises VALUES(12, 'Costa de Marfil');
-INSERT INTO Paises VALUES(13, 'Costa Rica');
-INSERT INTO Paises VALUES(14, 'Dinamarca');
-INSERT INTO Paises VALUES(15, 'República Dominicana');
-INSERT INTO Paises VALUES(16, 'Ecuador');
-INSERT INTO Paises VALUES(17, 'Egipto');
-INSERT INTO Paises VALUES(18, 'El Salvador');
-INSERT INTO Paises VALUES(19, 'Emiratos Árabes Unidos');
-INSERT INTO Paises VALUES(20, 'España');
-INSERT INTO Paises VALUES(21, 'Estados Unidos');
-INSERT INTO Paises VALUES(22, 'Finlandia');
-INSERT INTO Paises VALUES(23, 'Francia');
-INSERT INTO Paises VALUES(24, 'Grecia');
-INSERT INTO Paises VALUES(25, 'Guatemala');
-INSERT INTO Paises VALUES(26, 'Hong Kong');
-INSERT INTO Paises VALUES(27, 'Hungría');
-INSERT INTO Paises VALUES(28, 'India');
-INSERT INTO Paises VALUES(29, 'Indonesia');
-INSERT INTO Paises VALUES(30, 'Irán');
-INSERT INTO Paises VALUES(31, 'Iraq');
-INSERT INTO Paises VALUES(32, 'Irlanda');
-INSERT INTO Paises VALUES(33, 'Israel');
-INSERT INTO Paises VALUES(34, 'Italia');
-INSERT INTO Paises VALUES(35, 'Jamaica');
-INSERT INTO Paises VALUES(36, 'Japón');
-INSERT INTO Paises VALUES(37, 'Luxemburgo');
-INSERT INTO Paises VALUES(38, 'Macao');
-INSERT INTO Paises VALUES(39, 'Marruecos');
-INSERT INTO Paises VALUES(40, 'México');
-INSERT INTO Paises VALUES(41, 'Noruega');
-INSERT INTO Paises VALUES(42, 'Nueva Zelanda');
-INSERT INTO Paises VALUES(43, 'Paraguay');
-INSERT INTO Paises VALUES(44, 'Perú');
-INSERT INTO Paises VALUES(45, 'Portugal');
-INSERT INTO Paises VALUES(46, 'Puerto Rico');
-INSERT INTO Paises VALUES(47, 'Qatar');
-INSERT INTO Paises VALUES(48, 'Reino Unido');
-INSERT INTO Paises VALUES(4, 'Rusia');
-INSERT INTO Paises VALUES(50, 'Singapur');
-INSERT INTO Paises VALUES(51, 'Sudáfrica');
-INSERT INTO Paises VALUES(52, 'Suecia');
-INSERT INTO Paises VALUES(53, 'Suiza');
-INSERT INTO Paises VALUES(54, 'Tailandia');
-INSERT INTO Paises VALUES(55, 'Taiwán');
-INSERT INTO Paises VALUES(56, 'Tanzania');
-INSERT INTO Paises VALUES(57, 'Turquía');
-INSERT INTO Paises VALUES(58, 'Ucrania');
-INSERT INTO Paises VALUES(59, 'Uganda');
-INSERT INTO Paises VALUES(60, 'Uruguay');
-INSERT INTO Paises VALUES(61, 'Venezuela');
-INSERT INTO Paises VALUES(62,'Alemania');
+INSERT INTO Paises VALUES(9, 'Estados Unidos');
+
+delete from Paises
+select * from Paises
 
 --Fabricantes
 INSERT INTO Fabricantes (codFab, nomFab, dirFab, mailFab, emplFab) VALUES('VK','Volkswagen','Wolfsburgo, Alemania','info@volkswagen.com',3000);
@@ -218,27 +168,58 @@ INSERT INTO Fabricantes (codFab, nomFab, dirFab, mailFab, emplFab) VALUES('VV', 
 INSERT INTO Fabricantes (codFab, nomFab, dirFab, mailFab, emplFab) VALUES('YH', 'Yamaha', 'Iwata, Shizuoka, Japón', 'info@yamaha.com', 9002);	
 INSERT INTO Fabricantes (codFab, nomFab, dirFab, mailFab, emplFab) VALUES('HN', 'Harley-Davidson', 'Milwaukee, WI, Estados Unidos','info@harleydavidson.com', 11233);
 
+
 --Plantas
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(125483,'VK','Planta Volkswagen','Algo algo algo',	62,'planta@volkswagen.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(899871,'BM','Planta BMW','Algo algo algo', 62,'planta@bmw.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(343687,'CH', 'Planta Chevrolet','Algo algo algo',21,'planta@chevrolet.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(131894,'CI','Planta Citroen','Algo algo algo',23,'planta@citroen.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(641245,'FI','Planta Fiat','Algo algo algo',34,'planta@fiat.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(212318,'FD', 'Planta Ford','Algo algo algo',21,'planta@ford.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(789456,'HD','Planta Honda','Algo algo algo',36,'planta@honda.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(785825,'HY','Planta Hyundai','Algo algo algo',11,'planta@hyundai.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(125434,'JP','Planta Jeep','Algo algo algo',21,'planta@jeep.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(521565,'MB','Planta Mercedez Benz','Algo algo algo',62,'planta@mercedezbenz.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(289734,'NS','Planta Nissan','Algo algo algo',36,'planta@nissan.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(842567,'RT','Planta Renault','Algo algo algo',23,'planta@renault.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(645273,'SK','Planta Suzuki','Algo algo algo',36,'planta@suzuki.com');
-INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(945472,'HN','Planta Harley-Davidson','Algo algo algo',21,'planta@harleydevidson.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(125483,'VK','Planta Volkswagen','Algo algo algo',7,'planta@volkswagen.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(899871,'BM','Planta BMW','Algo algo algo',2,'planta@bmw.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(343687,'CH', 'Planta Chevrolet','Algo algo algo',9,'planta@chevrolet.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(131894,'CI','Planta Citroen','Algo algo algo',6,'planta@citroen.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(641245,'FI','Planta Fiat','Algo algo algo',3,'planta@fiat.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(212318,'FD', 'Planta Ford','Algo algo algo',9,'planta@ford.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(789456,'HD','Planta Honda','Algo algo algo',5,'planta@honda.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(785825,'HY','Planta Hyundai','Algo algo algo',8,'planta@hyundai.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(125434,'JP','Planta Jeep','Algo algo algo',9,'planta@jeep.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(521565,'MB','Planta Mercedez Benz','Algo algo algo',9,'planta@mercedezbenz.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(289734,'NS','Planta Nissan','Algo algo algo',5,'planta@nissan.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(842567,'RT','Planta Renault','Algo algo algo',6,'planta@renault.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(645273,'SK','Planta Suzuki','Algo algo algo',5,'planta@suzuki.com');
+INSERT INTO Plantas (codPlan, codFab, nomPlan, dirPlan, codPais, mailPlan) VALUES(945472,'HN','Planta Harley-Davidson','Algo algo algo',9,'planta@harleydevidson.com');
+
 
 -- Vehiculos
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('3FAHP0HA7AR316241','FUSION','VERDE',1134,'SEDAN 2.5', 40,'FD');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1J4FA24158L541376','WRANGLER','AZUL',1530,'2DR SUV 3.8 V8',21,'JP');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1C6RR7GT1ES409326','RAM PICKUP', 'NEGRO', 2581,'4DR PICKUP 5.7 V8',21,'DD');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1GBJC34R1YF432082','CK3500', 'AZUL',2209,'COUPE 2-DR V-8 6.2L',21,'CH');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1G1YY25R695700001','CORVETTE','VIOLETA', 1511,'COUPE 2-DR V-8 6.2L',21,'CH');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1FDXE45S53HC05364','VAN','BLANCO',1696,'V-10 6.8L 6.8L V10',21,'FD');
-INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1GNEK13ZX3R298984','Tahoe','AZUL',2419,'4WD 5.3L V8 OHV 16V FFV',21,'CH');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('3FAHP0HA7AR316241','FUSION','VERDE',1134,'SEDAN 2.5', 7,'FD');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1J4FA24158L541376','WRANGLER','AZUL',1530,'2DR SUV 3.8 V8',9,'JP');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1C6RR7GT1ES409326','RAM PICKUP', 'NEGRO', 2581,'4DR PICKUP 5.7 V8',9,'DD');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1GBJC34R1YF432082','CK3500', 'AZUL',2209,'COUPE 2-DR V-8 6.2L',9,'CH');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1G1YY25R695700001','CORVETTE','VIOLETA', 1511,'COUPE 2-DR V-8 6.2L',9,'CH');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1FDXE45S53HC05364','VAN','BLANCO',1696,'V-10 6.8L 6.8L V10',9,'FD');
+INSERT INTO Vehiculos(vin, modelo, color, peso, caracteristicas, codPais, codFab) VALUES('1GNEK13ZX3R298984','Tahoe','AZUL',2419,'4WD 5.3L V8 OHV 16V FFV',9,'CH');
+
+
+--Envios
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20030118',1191,7, 1);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20060729', 1607,9, 1);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20081001', 2710,9, 3);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20100320', 2319,9, 9);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20120516',1587,9, 8);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20150421',1781,9, 4);
+INSERT INTO Envios (fchEnvio,pesoEnvio,oriEnvio,desEnvio) VALUES('20170814',2540,9, 7);
+
+--Carga
+
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(15,000001,'3FAHP0HA7AR316241',1134);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(15,000001,'3FAHP0HA7AR316241',57);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(16,000002,'1J4FA24158L541376',1530);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(16,000002,'1J4FA24158L541376',77);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(17,000003,'1C6RR7GT1ES409326',2581);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(17,000003,'1C6RR7GT1ES409326',129);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(18,000004,'1GBJC34R1YF432082',2209);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(18,000004,'1GBJC34R1YF432082',110);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(19,000005,'1G1YY25R695700001',1511);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(19,000005,'1G1YY25R695700001',76);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(20,000006,'1FDXE45S53HC05364',1696);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(20,000006,'1FDXE45S53HC05364',85);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(21,000007,'1GNEK13ZX3R298984',2419);
+INSERT INTO Carga (idEnvio, idCarga, vin,pesoCarga) VALUES(21,000007,'1GNEK13ZX3R298984',121);
+
+select * from Carga
